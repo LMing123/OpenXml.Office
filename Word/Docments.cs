fb890b25da3b742672167c00b1f33078e2540a0a
XiaoMing <z971130192@live.com>
2017-06-17 23:35:00 +0800

    change a way that implement self define paragraphstyle

--- Word/Docments.cs
+++ Word/Docments.cs
@@ -18,8 +18,8 @@ namespace Word
         WordprocessingDocument doc;
         MainDocumentPart mainPart;
         Body body;
-       
-        public  Docments(string path, string docName)
+
+        public Docments(string path, string docName)
         {
             if (!File.Exists(path))
             {
@@ -39,7 +39,7 @@ namespace Word
                 {
                     mainPart = doc.MainDocumentPart;
                 }
-                if (mainPart.Document==null)
+                if (mainPart.Document == null)
                 {
                     mainPart.Document = new Document();
                 }
@@ -47,7 +47,7 @@ namespace Word
                 {
                     mainPart.Document = new Document();
                 }
-                if (mainPart.Document.Body==null)
+                if (mainPart.Document.Body == null)
                 {
                     body = mainPart.Document.AppendChild(new Body());
                 }
@@ -56,119 +56,56 @@ namespace Word
                     body = mainPart.Document.Body;
                 }
             }
-            
+
         }
         public void AddParagraph()
         {
 
             Paragraph para = body.AppendChild(new Paragraph());
             Run run = para.AppendChild(new Run());
-           
+
             run.AppendChild(new Text("\n"));
 
         }
 
-        public void AddParagraph(string text, FormatCollection fc=null)
-        {     
-           
+        public void AddParagraph(string text, FormatCollection fc = null)
+        {
+
             Paragraph para = body.AppendChild(new Paragraph());
             para.ParagraphProperties = new ParagraphProperties();
             Run run = para.AppendChild(new Run());
-            if (fc!=null)
+            if (fc != null)
             {
                 para.ParagraphProperties = fc.ParagraphProperties;
                 run.RunProperties = fc.RunProperties;
             }
-            
-            
+
+
             run.AppendChild(new Text(text));
-           
+
         }
 
         public void AddText(string text)
         {
-           var m= body.ChildElements.Where(i=>i.LocalName=="p");
-           m.Last().AppendChild(new Run(new Text(text)));
+            var m = body.ChildElements.Where(i => i.LocalName == "p");
+            m.Last().AppendChild(new Run(new Text(text)));
         }
 
-        public  void CreateParagraphStyle( string styleID, string styleName, string aliases = "")
+        public void CreateParagraphStyle(string styleID, string styleName, string aliases = "")
         {
-            Styles styles = mainPart.StyleDefinitionsPart.Styles;
-            if (styles == null)
-            {
-                mainPart.StyleDefinitionsPart.Styles = new Styles();
-                mainPart.StyleDefinitionsPart.Styles.Save();
-            }
-            Style style = new Style
-            {
-                Type = StyleValues.Paragraph,
-                StyleId = styleID,
-                CustomStyle = true,
-                Default = false
-            };
-            // Create and add the child elements (properties of the style).
-            Aliases aliases1 = new Aliases() { Val = aliases };
-            AutoRedefine autoredefine1 = new AutoRedefine() { Val = OnOffOnlyValues.Off };
-            BasedOn basedon1 = new BasedOn() { Val = "Normal" };
-            LinkedStyle linkedStyle1 = new LinkedStyle() { Val = "OverdueAmountChar" };
-            Locked locked1 = new Locked() { Val = OnOffOnlyValues.Off };
-            PrimaryStyle primarystyle1 = new PrimaryStyle() { Val = OnOffOnlyValues.On };
-            StyleHidden stylehidden1 = new StyleHidden() { Val = OnOffOnlyValues.Off };
-            SemiHidden semihidden1 = new SemiHidden() { Val = OnOffOnlyValues.Off };
-            StyleName styleName1 = new StyleName() { Val = styleName };
-            NextParagraphStyle nextParagraphStyle1 = new NextParagraphStyle() { Val = "Normal" };
-            UIPriority uipriority1 = new UIPriority() { Val = 1 };
-            UnhideWhenUsed unhidewhenused1 = new UnhideWhenUsed() { Val = OnOffOnlyValues.On };
-            if (aliases != "")
-                style.Append(aliases1);
-            style.Append(autoredefine1);
-            style.Append(basedon1);
-            style.Append(linkedStyle1);
-            style.Append(locked1);
-            style.Append(primarystyle1);
-            style.Append(stylehidden1);
-            style.Append(semihidden1);
-            style.Append(styleName1);
-            style.Append(nextParagraphStyle1);
-            style.Append(uipriority1);
-            style.Append(unhidewhenused1);
-
-            // Create the StyleRunProperties object and specify some of the run properties.
-            StyleRunProperties styleRunProperties1 = new StyleRunProperties();
-            Bold bold1 = new Bold();
-            Color color1 = new Color() { ThemeColor = ThemeColorValues.Accent2 };
-            RunFonts font1 = new RunFonts() { Ascii = "Lucida Console" };
-            Italic italic1 = new Italic();
-            // Specify a 12 point size.
-            FontSize fontSize1 = new FontSize() { Val = "24" };
-            styleRunProperties1.Append(bold1);
-            styleRunProperties1.Append(color1);
-            styleRunProperties1.Append(font1);
-            styleRunProperties1.Append(fontSize1);
-            styleRunProperties1.Append(italic1);
-
-            // Add the run properties to the style.
-            style.Append(styleRunProperties1);
-
-            // Add the style to the styles part.
-            styles.Append(style);
-
+            CreateAndAddParagraphStyle.CreateParagraphStyle(mainPart.StyleDefinitionsPart, styleID, styleName, aliases);
         }
-        
-        public  StyleDefinitionsPart AddStylesPartToPackage(WordprocessingDocument doc=null)
+
+        public StyleDefinitionsPart AddStylesPartToPackage(WordprocessingDocument doc = null)
         {
             doc = this.doc;
-           StyleDefinitionsPart part;
-            part = doc.MainDocumentPart.AddNewPart<StyleDefinitionsPart>();
-            Styles root = new Styles();
-            root.Save(part);
-            return part;
+            return CreateAndAddParagraphStyle.AddStylesPartToPackage(doc);
         }
         public void Close()
         {
             doc.Close();
         }
 
-        
+
     }
 }
